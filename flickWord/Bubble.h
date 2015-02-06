bd3af206b023919134d9f7934d6a1ff83309b48f
SangChan Lee <sangchan82@gmail.com>
2015-02-06 18:10:02 +0900

    make bubble object

---
+++ flickWord/Bubble.h
@@ -0,0 +1,13 @@
+//
+//  Bubble.h
+//  flickWord
+//
+//  Created by SangChan on 2015. 2. 6..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Bubble : SKSpriteNode
+
+@end
---
+++ flickWord/Bubble.m
@@ -0,0 +1,13 @@
+//
+//  Bubble.m
+//  flickWord
+//
+//  Created by SangChan on 2015. 2. 6..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import "Bubble.h"
+
+@implementation Bubble
+
+@end
