c674c0542695ece479a00ba42b95333810d2e2af
SangChan Lee <sangchan82@gmail.com>
2015-05-25 17:46:41 +0900

    add watch app extension

---
+++ flickWord WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  flickWord WatchKit Extension
+//
+//  Created by LeeSangchan on 2015. 5. 25..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ flickWord WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  flickWord WatchKit Extension
+//
+//  Created by LeeSangchan on 2015. 5. 25..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
