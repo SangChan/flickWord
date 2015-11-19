e54ba316559cea8787582fc8ad4a2c726b5390ee
Sangchan Lee <sangchan.lee@ef.com>
2015-11-19 16:28:22 +0800

    add runnable UI test cases on this

--- flickWordUITests/flickWordUITests.m
+++ flickWordUITests/flickWordUITests.m
@@ -1,8 +1,8 @@
 //
 //  flickWordUITests.m
-//  flickWord
+//  flickWordUITests
 //
-//  Created by SangChan Lee on 11/17/15.
+//  Created by SangChan Lee on 11/19/15.
 //  Copyright © 2015 sangchan. All rights reserved.
 //
 
@@ -23,7 +23,7 @@
     self.continueAfterFailure = NO;
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
     [[[XCUIApplication alloc] init] launch];
-
+    
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
 
@@ -35,6 +35,18 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
     
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElement *element = [[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element;
+    [element tap];
+    [element tap];
+    [element tap];
+    [element tap];
+    [element tap];
+    [app.buttons[@"\Uf053"] tap];
+
 }
+
 @end
